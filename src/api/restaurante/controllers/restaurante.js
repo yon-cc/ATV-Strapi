32ae12653eeaa028d4ec56b29bab24358b55925f
Jhon Jairo Corzo Calderon <jhon.corzo.2020@upb.edu.co>
2025-01-10 11:37:29 -0500

    delete restaurants

--- src/api/restaurante/controllers/restaurante.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * restaurante controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::restaurante.restaurante');
--- src/api/restaurante/services/restaurante.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * restaurante service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::restaurante.restaurante');
